fe98816c90e5ad297792944bbd1d22b1c104ff3e
Vallent12 <fevadtya09.gmail.com>
2025-11-29 19:36:10 +0700

    Tugas 3 B

--- RekapPenjualanCafe03.java
+++ RekapPenjualanCafe03.java
@@ -1,17 +1,17 @@
 import java.util.Scanner;
 public class RekapPenjualanCafe03 {
 
-public static String[] menu = {"Kopi", "Teh", "Es Kelapa Muda", "Roti Bakar", "Gorengan"};
+public static String[] menu;
 
-    public static int[][] inputPenjualan() {
+    public static int[][] inputPenjualan(int jumlahMenu, int jumlahHari) {
         Scanner input = new Scanner(System.in);
 
-        int[][] penjualan = new int[5][7];
+        int[][] penjualan = new int[jumlahMenu][jumlahHari];
 
         System.out.println("\n== INPUT DATA PENJUALAN ==");
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < jumlahMenu; i++) {
             System.out.println("\nMenu: " + menu[i]);
-            for (int j = 0; j < 7; j++) {
+            for (int j = 0; j < jumlahHari; j++) {
                 System.out.print("Hari ke-" + (j + 1) + ": ");
                 penjualan[i][j] = input.nextInt();
             }
@@ -20,30 +20,30 @@ public static String[] menu = {"Kopi", "Teh", "Es Kelapa Muda", "Roti Bakar", "G
         return penjualan;
     }
 
-    public static void tampilTabel(int[][] p) {
+    public static void tampilTabel(int[][] p, int jumlahMenu, int jumlahHari) {
         System.out.println("\n=== TABEL PENJUALAN ===");
         System.out.printf("%-15s", "Menu");
-        for (int h = 1; h <= 7; h++) {
+        for (int h = 1; h <= jumlahHari; h++) {
             System.out.printf("Hari %d\t", h);
         }
         System.out.println();
 
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < jumlahMenu; i++) {
             System.out.printf("%-15s", menu[i]);
-            for (int j = 0; j < 7; j++) {
+            for (int j = 0; j < jumlahHari; j++) {
                 System.out.printf("%d\t", p[i][j]);
             }
             System.out.println();
         }
     }
 
-    public static void tampilMenuTertinggi(int[][] p) {
+    public static void tampilMenuTertinggi(int[][] p, int jumlahMenu, int jumlahHari) {
         int max = 0;
         int indexMenu = 0;
 
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < jumlahMenu; i++) {
             int total = 0;
-            for (int j = 0; j < 7; j++) {
+            for (int j = 0; j < jumlahHari; j++) {
                 total += p[i][j];
             }
             if (total > max) {
@@ -57,28 +57,41 @@ public static String[] menu = {"Kopi", "Teh", "Es Kelapa Muda", "Roti Bakar", "G
         System.out.println("Total penjualan: " + max);
     }
 
-    public static void tampilRataRata(int[][] p) {
+    public static void tampilRataRata(int[][] p, int jumlahMenu, int jumlahHari) {
         System.out.println("\n== RATA-RATA PENJUALAN SETIAP MENU ==");
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < jumlahMenu; i++) {
             int total = 0;
-            for (int j = 0; j < 7; j++) {
+            for (int j = 0; j < jumlahHari; j++) {
                 total += p[i][j];
             }
-            double rata = total / 7.0;
+            double rata = total / (double) jumlahHari;
 
             System.out.println(menu[i] + " : " + rata);
         }
     }
 
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
 
-        int[][] data = inputPenjualan();
+        System.out.print("Masukkan jumlah menu: ");
+        int jumlahMenu = input.nextInt();
 
-        tampilTabel(data);
+        System.out.print("Masukkan jumlah hari penjualan: ");
+        int jumlahHari = input.nextInt();
+        input.nextLine();
 
-        tampilMenuTertinggi(data);
+        menu = new String[jumlahMenu];
+        System.out.println("\n== iNPUT NAMA MENU ==");
+        for (int i = 0; i < jumlahMenu; i++) {
+            System.out.print("Nama menu ke-" + (i + 1) + ": ");
+            menu[i] = input.nextLine();
+        }
 
-        tampilRataRata(data);
+        int [][] data = inputPenjualan(jumlahMenu, jumlahHari);
+
+        tampilTabel(data, jumlahMenu, jumlahHari);
+        tampilMenuTertinggi(data, jumlahMenu, jumlahHari);
+        tampilRataRata(data, jumlahMenu, jumlahHari);
     }
     
 }
