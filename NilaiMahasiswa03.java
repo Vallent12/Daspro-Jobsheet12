55513d602c7625d5f1cabda99cc11a7afe986425
Vallent12 <fevadtya09.gmail.com>
2025-11-29 18:42:02 +0700

    Main

--- NilaiMahasiswa03.java
+++ NilaiMahasiswa03.java
@@ -13,6 +13,13 @@ public class NilaiMahasiswa03 {
         return nilai;
     }
 
+    public static void tampilArray(int[] nilai) {
+        System.out.println("\n==== Daftar Nilai Mahasiswa ====");
+        for (int i = 0; i < nilai.length; i++) {
+            System.out.println("Mahasiswa ke-" + (i + 1) + " : " + nilai[i]);
+        }
+    }
+
     public static int hitTot(int[] nilai) {
         int total = 0;
 
@@ -30,7 +37,9 @@ public class NilaiMahasiswa03 {
         int N = sc.nextInt();
 
         int[] nilaiMahasiswa = isianArray(N);
-        
+
+        tampilArray(nilaiMahasiswa);
+
         int totalNilai = hitTot(nilaiMahasiswa);
 
         System.out.println("\nTotal nilai seluruh mahasiswa = " + totalNilai);
