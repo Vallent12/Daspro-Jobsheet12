718bf97504f16bcfc1ca73259833f079a911def9
Vallent12 <fevadtya09.gmail.com>
2025-11-29 16:54:44 +0700

    Modifikasi percobaan 5

--- HitungBalok03.java
+++ HitungBalok03.java
@@ -1,8 +1,19 @@
 import java.util.Scanner;
 public class HitungBalok03 {
 
+    static int hitungLuas(int pjg, int lb) {
+        int Luas = pjg * lb;
+        return Luas;
+    }
+
+    static int hitungVolume(int tinggi, int a, int b) {
+        int volume = hitungLuas(a, b) * tinggi;
+        return volume;
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         int p, l, t, L, vol;
 
         System.out.println("Masukkan panjang");
@@ -14,9 +25,10 @@ public class HitungBalok03 {
         System.out.println("Masukkan tinggi");
         t = sc.nextInt();
 
-        L = p * l;
+        L = hitungLuas(p, l);
         System.out.println("Luas persegi panjang adalah " + L);
-        vol = p * l * t;
+
+        vol = hitungVolume(t, p, l);
         System.out.println("Volume balok adalah " + vol);
 
         sc.close();
